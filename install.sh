f843f82a60eb56351d181bdb2f2db290fbd9fa03
Jonathan Helmus <jjhelmus@gmail.com>
2015-02-03 15:41:11 -0600

    Debugging

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
+set -x
 MINICONDA_URL="http://repo.continuum.io/miniconda"
 MINICONDA_FILE="Miniconda3-3.8.3-MacOSX-x86_64.sh"
 wget "${MINICONDA_URL}/${MINICONDA_FILE}"
 bash $MINICONDA_FILE -b
 
 export PATH=/Users/travis/miniconda/bin:$PATH
-
+ls /Users/travis/miniconda/bin
 conda update --yes conda
 conda install --yes pip conda-build jinja2 binstar
