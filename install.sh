15891623d3865bf05e10ddf656e0a2c6ef4a6d29
Jonathan Helmus <jjhelmus@gmail.com>
2016-02-05 11:03:34 -0600

    Py-ART v1.6.0 release
    Python 2.6, 2.7, 3.3, 3.4, 3.5 NumPy 1.9

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 MINICONDA_URL="http://repo.continuum.io/miniconda"
-MINICONDA_FILE="Miniconda-3.8.3-MacOSX-x86_64.sh"
+MINICONDA_FILE="Miniconda-latest-MacOSX-x86_64.sh"
 wget "${MINICONDA_URL}/${MINICONDA_FILE}"
 bash $MINICONDA_FILE -b
 
@@ -7,7 +7,7 @@ export PATH=/Users/travis/miniconda/bin:$PATH
 
 conda update --yes conda
 conda install --yes conda-build jinja2 anaconda-client
-conda config --add channels https://conda.binstar.org/jjhelmus
+conda config --add channels https://conda.anaconda.org/jjhelmus
 
 # Remove gfortran so that all fortran Py-ART modules are not built.
 # This avoids the need to package the gfortran run time in the conda package.
